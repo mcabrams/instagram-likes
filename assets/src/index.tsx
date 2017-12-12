a577008e11a90e547c16e8d24d56c1ddb9cb630a
Matthew Abrams <mcabrams1@gmail.com>
2017-12-11 22:46:03 -0800

    Add redux WIP

--- assets/src/index.tsx
+++ assets/src/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import { App } from './components/App';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-    <App appName="Instagram Likes"/>,
+    <Provider store={store}>
+      <App appName="Instagram Likes" />
+    </Provider>,
     document.getElementById('app'),
 );
